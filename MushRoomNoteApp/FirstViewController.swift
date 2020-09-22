cba4ea49e5faa43c2ba4361e0c562177ac7680a0
risa.takahashi <optim.takahashi.demo@gmail.com>
2020-09-22 21:40:02 +0900

    Initial Commit

---
+++ MushRoomNoteApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MushRoomNoteApp
+//
+//  Created by Risa Takahashi on 2020/09/22.
+//  Copyright © 2020 Risa Takahashi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MushRoomNoteApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MushRoomNoteApp
+//
+//  Created by Risa Takahashi on 2020/09/22.
+//  Copyright © 2020 Risa Takahashi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
